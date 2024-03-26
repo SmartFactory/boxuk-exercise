aa182c076d2b768e9a189c6ea6c83471f43106c6
alexfarrbox <35072391+alexfarrbox@users.noreply.github.com>
2024-03-26 12:43:09 +0000

    [MINOR] fix the 404 port in url issue, and base theme

---
+++ drupal/web/themes/box_test_theme/js/box-test-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Box Test Theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.boxTestTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
